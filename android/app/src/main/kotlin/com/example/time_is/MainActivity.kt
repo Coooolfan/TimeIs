5cc95c7d3f0cd5328a07e16520adff8e86371a15
YangYiFan <Coolfan1024@gmail.com>
2024-07-25 23:39:57 +0800

    fix: 获取GNSS原始数据

--- android/app/src/main/kotlin/com/example/time_is/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/time_is/MainActivity.kt
@@ -1,5 +1,63 @@
 package com.example.time_is
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.location.GnssClock
+import android.location.GnssStatus
+import android.location.LocationManager
+import android.os.BatteryManager
+import android.os.Build
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.annotation.RequiresApi
+import java.sql.Time
+import java.time.LocalTime
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    private val BATTERY_CHANNEL  = "com.coooolfan.timeis/battery"
+    private val TIME_CHANNEL  = "com.coooolfan.timeis/time"
+
+    @RequiresApi(VERSION_CODES.O)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, BATTERY_CHANNEL).setMethodCallHandler {
+                call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, TIME_CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getPlatformTime") {
+                result.success(getPlatformTime())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+        batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+
+        return batteryLevel
+    }
+    @RequiresApi(VERSION_CODES.O)
+    private fun getPlatformTime():String{
+        return LocalTime.now().toString()
+    }
+}
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:raw_gnss/gnss_measurement_model.dart';
 import 'package:raw_gnss/raw_gnss.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
-
   final String title = "TimeIs Demo";
 
   @override
@@ -16,22 +14,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const platformBattery = MethodChannel('com.coooolfan.timeis/battery');
+  static const platformTime = MethodChannel('com.coooolfan.timeis/time');
   String _timeString = "00:00:00";
-  String _locationMsg = "Location is: ";
   late Timer _timer;
-  Position _position = Position(
-      latitude: 0,
-      longitude: 0,
-      accuracy: 0,
-      altitude: 0,
-      heading: 0,
-      speed: 0,
-      speedAccuracy: 0,
-      timestamp: DateTime.now(),
-      floor: 0,
-      isMocked: false,
-      altitudeAccuracy: 0,
-      headingAccuracy: 0);
+
+  String _batteryLevel = 'Unknown battery level.';
+  String _platformTimeString = 'Unknown time';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platformBattery.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    } on MissingPluginException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  Future<void> _getTime() async {
+    String platformTimeString;
+    try {
+      final result = await platformTime.invokeMethod<String>('getPlatformTime');
+      platformTimeString = 'Time is $result.';
+    } on PlatformException catch (e) {
+      platformTimeString = "Failed to get time : '${e.message}'.";
+    } on MissingPluginException catch (e) {
+      platformTimeString = "Failed to get time : '${e.message}'.";
+    }
+
+    setState(() {
+      _platformTimeString = platformTimeString;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -45,14 +67,6 @@ class _MyHomePageState extends State<MyHomePage> {
     PermissionStatus status = await permission.status;
     print('检测权限$status');
     if (status.isGranted) {
-      Position? position = await Geolocator.getLastKnownPosition();
-      if (position != null) {
-        setState(() {
-          _locationMsg = "Last Location is: ";
-          _position = position;
-        });
-      }
-
       //权限通过
     } else if (status.isDenied) {
       //权限拒绝， 需要区分IOS和Android，二者不一样
@@ -75,55 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
     if (!status.isGranted) {
       openAppSettings();
     }
-  }
-
-  void _refreshLocation() async {
-    setState(() {
-      _locationMsg = "Location is loding: ";
-    });
-    Position position = await _determinePosition();
-    print('Location: ${position.latitude}, ${position.longitude}');
-    setState(() {
-      _position = position;
-      _locationMsg = "Location is: ";
-    });
-  }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
   }
 
   void _startTimer() {
@@ -174,24 +139,21 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Text(
-              _locationMsg,
+              _batteryLevel,
               style: const TextStyle(
-                fontSize: 30,
+                fontSize: 15,
                 fontWeight: FontWeight.normal,
               ),
             ),
-            Text(
-              _position.toString(),
-              style: const TextStyle(
-                fontSize: 15,
-              ),
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
             ),
-              StreamBuilder<GnssMeasurementModel>(
+            StreamBuilder<GnssMeasurementModel>(
               builder: (context, snapshot) {
                 if (snapshot.data == null) {
                   return const CircularProgressIndicator();
                 }
-
                 return ListView.builder(
                   itemBuilder: (context, position) {
                     return ListTile(
@@ -207,11 +169,11 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refreshLocation,
-        tooltip: 'RefreshLocation',
-        child: const Icon(Icons.refresh),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _refreshLocation,
+      //   tooltip: 'RefreshLocation',
+      //   child: const Icon(Icons.refresh),
+      // ),
     );
   }
 }
